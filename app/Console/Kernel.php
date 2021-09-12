1d932ab28c9ca47209ccb64c2f6ad737114dc99e
Guilherme Capanema de Barros <guilherme@capanema.me>
2021-09-12 12:14:26 -0300

    Add missing import to kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use App\Jobs\TakeSnapshot;
+use App\Models\Website;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
