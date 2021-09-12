3652e4df32bb43687dd65a15e7b16b3ccfb3ca93
Guilherme Capanema de Barros <guilherme@capanema.me>
2021-09-12 11:47:01 -0300

    Fix route service provier home

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/websites';
+    public const HOME = '/';
 
     /**
      * The controller namespace for the application.
